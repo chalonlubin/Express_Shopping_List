7b45d14090d99042de301060832f14e21a0e08eb
Chalon Lubin <chalonlubin1@gmail.com>
2023-01-04 17:38:17 -0800

    final commit before class ends

--- itemRoutes.js
+++ itemRoutes.js
@@ -1,26 +1,27 @@
+/** Routes for shopping list app. */
+
 const express = require("express");
 
 const db = require("./fakeDb");
+const router = new express.Router();
 
-const router = new express.Router():
+// app.use(express.json()); // Do we need here?
 
-app.use(express.json());
-
+//** GET /items: get list of items */
 router.get("/", function(req, res) {
   return res.json({
     items: db.items
   })
 })
 
+//** POST /items: add item to list */
 router.post("/", function(req, res) {
-  name = req.body.name;
-  price = req.body.price;
+  const name = req.body.name;
+  const price = req.body.price;
 
   db.items.push({name, price})
 
   return res.json({"added": {name, price}})
-
-
 })
 
-module.export()
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const express = require("express");
 
-const app = express();
+const app = require("./app");
 
 app.listen(3000, function() {
-  console.log("App started at https://localhost:3000/");
+  console.log("App started at http://localhost:3000/");
 });
