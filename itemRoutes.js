e4cd96e697ae5de7e16c35bf88ddd2206e2bc77d
Chalon Lubin <chalonlubin1@gmail.com>
2023-01-05 00:06:10 -0800

    completed middleware.js

--- itemRoutes.js
+++ itemRoutes.js
@@ -5,6 +5,8 @@ const express = require("express");
 const db = require("./fakeDb");
 const router = new express.Router();
 
+const { finder, confirmType } = require("./middleware");
+
 
 //** GET /items: get list of items */
 router.get("/", function(req, res) {
@@ -15,7 +17,7 @@ router.get("/", function(req, res) {
 })
 
 //** POST /items: add item to list */
-router.post("/", function(req, res) {
+router.post("/", confirmType, function(req, res) {
   const { name, price } = req.body
 
   db.items.push({name, price})
@@ -24,7 +26,7 @@ router.post("/", function(req, res) {
 })
 
 //* GET /items/:name: get a single item. */
-router.get("/:name", function(req, res) {
+router.get("/:name", finder, function(req, res) {
 
   const result = db.items.find(({ name }) => name === req.params.name);
 
@@ -32,7 +34,7 @@ router.get("/:name", function(req, res) {
 })
 
 //* PATCH /items/:name: Edit a single item. */
-router.patch("/:name", function(req, res) {
+router.patch("/:name", finder, confirmType, function(req, res) {
   const { name, price } = req.body
   const index = db.items.findIndex(({ name }) => name === req.params.name);
 
@@ -42,7 +44,7 @@ router.patch("/:name", function(req, res) {
 })
 
 //* DELETE /items/:name: Delete a single item. */
-router.delete("/:name", function(req, res) {
+router.delete("/:name", finder, function(req, res) {
   const index = db.items.findIndex(({ name }) => name === req.params.name);
 
   db.items.splice(index,1);
--- middleware.js
+++ middleware.js
@@ -1,28 +1,33 @@
 /** Middleware for express-shopping-list app. */
 
 const { BadRequestError, NotFoundError } = require("./expressError");
-const { items } = require("./fakeDb")
+const db = require("./fakeDb");
 
 function finder(req, res, next) {
-
   if (db.items.find(({ name }) => name === req.params.name)) {
-    next()
+    next();
   } else {
-    throw new NotFoundError(`${req.name} is not an item.`)
+    throw new NotFoundError(`${req.params.name} is not an item.`);
   }
 }
 
 function confirmType(req, res, next) {
-  const { name, price } = req.body
-  try {
-   name.toString()
-  } catch(e) {
-    throw new BadRequestError("Please input valid alphabetical characters.")
+  const { name, price } = req.body;
+
+  // Make sure name is a-z and less than 25 chars long.
+
+  if (!/^[a-z ]+$/i.test(name)) {
+    throw new BadRequestError("Please input a valid string.")
   }
+
+  // Make sure price can be converted into a number.
   try {
-    Number(price)
-   } catch(e) {
-     throw new BadRequestError("Please input a valid number.")
-   }
-  next()
+    Number(price);
+  } catch (e) {
+    throw new BadRequestError("Please input a valid number.");
+  }
+  if (!Number(price)) throw new BadRequestError("Please input a valid number.");
+  next();
 }
+
+module.exports = { finder, confirmType };
